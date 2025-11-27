917b7330c95e9c1c0edf0f442eaa5f464816c321
mayeuticsapp <mail.r.murgia@gmail.com>
2025-11-27 14:49:50 +0100

    Implement save-menu function for POST requests

--- netlify/functions/save-menu.js
+++ netlify/functions/save-menu.js
@@ -1,1 +1,29 @@
+import { blobs } from "@netlify/blobs";
+
+export async function handler(event) {
+    if (event.httpMethod !== "POST") {
+        return {
+            statusCode: 405,
+            body: "Metodo non consentito"
+        };
+    }
+
+    try {
+        const menuData = event.body;
+
+        const storage = blobs();
+        await storage.set("menu.json", menuData);
+
+        return {
+            statusCode: 200,
+            body: "Menu salvato correttamente"
+        };
+
+    } catch (err) {
+        return {
+            statusCode: 500,
+            body: "Errore: " + err
+        };
+    }
+}
 
